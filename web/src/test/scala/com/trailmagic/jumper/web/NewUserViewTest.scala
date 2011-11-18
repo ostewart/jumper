cce1bce105cd3515c85ea747adaed83aaa334625
Oliver Stewart <oliver@trailmagic.com>
2011-11-17 19:32:20 -0500

    Add Jsoup selector assertions

--- web/src/test/scala/com/trailmagic/jumper/web/NewUserViewTest.scala
+++ web/src/test/scala/com/trailmagic/jumper/web/NewUserViewTest.scala
@@ -3,6 +3,7 @@ package com.trailmagic.jumper.web
 import org.junit.Assert._
 import org.junit.Test
 import org.springframework.mock.web.MockHttpServletRequest
+import org.jsoup.Jsoup
 
 
 class NewUserViewTest {
@@ -15,7 +16,8 @@ class NewUserViewTest {
     val output = viewHelper.layoutView(viewName, Map("errors" -> Map(),
                                                      "contextPath" -> "/foo",
                                                      "request" -> new MockHttpServletRequest))
-
-    assertTrue("Contains correct title", output.contains("<title>Jumper | Create Account</title>"))
+    val doc = Jsoup.parse(output)
+    assertEquals("Jumper | Create Account", doc.select("title").text)
+    assertEquals("Create an Account", doc.select("h1").text)
   }
 }
